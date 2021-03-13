01e1944358ba692712ed5a0fc5fcba004c9ef716
Richard van den Broek <vandenbroek.rp@gmail.com>
2021-03-13 10:57:48 -0300

    added performance backlogs, some more refactoring, async model prediction

--- js/app.js
+++ js/app.js
@@ -4,9 +4,7 @@ const annCanvas2 = document.getElementById('annCanvas2')
 const outputCanvas = document.getElementById('canvas_output')
 const txtCanvas = document.getElementById('txtCanvas')
 const faceCascadeFile = 'haarcascade_frontalface_default.xml'; // path to xml
-
 const FPS = 24;
-let face = false
 
 function openCvReady() {
   cv['onRuntimeInitialized'] = () => {
@@ -25,6 +23,7 @@ function openCvReady() {
       let cap = new cv.VideoCapture(cam_input);
       let faces = new cv.RectVector();
       let classifier = new cv.CascadeClassifier();
+      let mSize = new cv.Size((video.height / 4), (video.width / 4));
       let utils = new Utils('errorMessage');
       utils.createFileFromUrl(faceCascadeFile, faceCascadeFile, () => {
           classifier.load(faceCascadeFile); // in the callback, load the cascade from file
@@ -34,24 +33,34 @@ function openCvReady() {
         let begin = Date.now();
         cap.read(src);
         src.copyTo(dst);
+        console.log(`exec time ${Date.now() - begin} -- ## until detecting faces`)
         cv.cvtColor(dst, gray, cv.COLOR_RGBA2GRAY);
         try {
-            classifier.detectMultiScale(gray, faces, 1.1, 3, 0);
+            classifier.detectMultiScale(gray, faces, 1.1, 3, 0, mSize);
         } catch (err) {
             console.log(err);
         }
+        console.log(`exec time ${Date.now() - begin} -- ## until draw rectangles`)
         for (let i = 0; i < faces.size(); ++i) {
             face = faces.get(i);
             let point1 = new cv.Point(face.x, face.y);
             let point2 = new cv.Point(face.x + face.width, face.y + face.height);
             cv.rectangle(dst, point1, point2, [255, 0, 0, 255]);
-            get_prediction(face)
         }
+        console.log(`exec time ${Date.now() - begin} -- ## until predict`)
+        if (faces.size() > 0) {
+          getPrediction(faces.get(0))
+          console.log(`${faces.size()} faces detected`)
+        }
+        console.log(`exec time ${Date.now() - begin} -- ## until redraw`)
         cv.imshow("canvas_output", dst);
-
         let delay = 10000 / FPS - (Date.now() - begin);
         setTimeout(processVideo, delay);
+        console.log(`exec time ${Date.now() - begin} -- ## process video`)
+        console.log(`===============================`)
+
       }
+
     setTimeout(processVideo, 0);
   }
 };
--- js/model.js
+++ js/model.js
@@ -16,46 +16,55 @@ const colors_dict = {
 
 //Function to load model
 async function loadModel() {
+  let begin = Date.now();
   console.log("loading model....");
   model = await tf.loadLayersModel('saved_models/7813-bruno/model.json');
+  console.log(`exec time ${Date.now() - begin} -- loadModel`)
 }
 
-function annotateEmotionShapes(emotion){
+function annotateEmotionShapes(face, emotion){
+  let begin = Date.now();
   coordinates = [face.x, face.y, face.width, face.height]
   cornerCoordinates = [30, 40, 0, 0]
   annotateShapes(annCanvas1, coordinates, emotion, 'rgba(40,40,250,.2)')
   annotateShapes(annCanvas2, coordinates, emotion, 'rgba(40,40,250,.2)')
   annotateShapes(txtCanvas, cornerCoordinates, emotion, colors_dict[emotion])
+  console.log(`exec time ${Date.now() - begin} -- annotateEmotionShapes`)
 }
 
-function get_prediction(face) {
-  snapImageData(face).then(function (imgData) {
-    let tensor = prepare_tensor(imgData);
-    console.log(tensor.shape)
-    let prediction = model.predict(tensor);
-    let indexMax = indexOfMax(prediction.dataSync())
-    emotion = prediction_dict[indexMax]
-    annotateEmotionShapes(emotion)
-  });
+async function getPrediction(face) {
+  // predicts a batch of samples
+  // model.predictOnBatch(tf.ones([8, 10])).print();
+  let pixels = snapImageData(face)
+  let tensor = prepareTensor(pixels);
+  let begin = Date.now();
+  let prediction = await model.predict(tensor);
+  console.log(`exec time ${Date.now() - begin} -- getPrediction`)
+  let indexMax = indexOfMax(prediction.dataSync())
+  emotion = prediction_dict[indexMax]
+  annotateEmotionShapes(face, emotion)
 }
 
 function snapImageData(face) {
-  return new Promise(function (resolve, reject) {
-    if (face == null) return reject();
-    context = outputCanvas.getContext('2d')
-    resolve(context.getImageData(face.x, face.y, face.width, face.height));
-  });
+  let begin = Date.now();
+  let context = outputCanvas.getContext('2d')
+  let pixels = context.getImageData(face.x, face.y, face.width, face.height);
+  console.log(`exec time ${Date.now() - begin} -- snapImageData`)
+  return pixels
 }
 
-function prepare_tensor(pixel_array) {
-  let a = tf.browser.fromPixels(pixel_array, 1)
-  let resized = tf.image.resizeBilinear(a.div(255), [48, 48]);
-  tensor = resized.expandDims(0);
+function prepareTensor(pixel_array) {
+  let begin = Date.now();
+  let tensor = tf.browser.fromPixels(pixel_array, 1)
+  tensor = tf.image.resizeBilinear(tensor.div(255), [48, 48]);
+  tensor = tensor.expandDims(0);
+  console.log(`exec time ${Date.now() - begin} -- prepareTensor`)
   return tensor
 }
 
 // Function to get the index of the maximum value of an array
 function indexOfMax(arr) {
+  let begin = Date.now();
     if (arr.length === 0) {
         return -1;
     }
@@ -67,5 +76,6 @@ function indexOfMax(arr) {
             max = arr[i];
         }
     }
+    console.log(`exec time ${Date.now() - begin} -- indexOfMax`)
     return maxIndex;
 }
